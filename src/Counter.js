5b05cb116ef913ba20ed948173a46b781b1c1f4a
신재우 <sinjaewoo@sinjaeuui-MacBookPro.local>
2021-12-12 20:50:48 +0900

    - counter.js 생성자 통해서 state 설정

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,33 @@
+/**
+ * 리액트에서 state는 바뀔 수 있는 값을 의미합니다.
+ * class형 컴포넌트 state
+ * function형 컴포넌트는 useState
+ *
+ * super 함수를 호출하면 현재 클래스형 컴포넌트가 상속받고 있는 리액트의 Component 클래스가 지닌 생성자 함수를 호출해 줍니다.
+ * 컴포넌트의 state는 객체 형식이어야 합니다.
+ */
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  // state의 초기값 설정
-  state = {
-    number: 0,
-    fixedNumber: 0,
-  };
+  constructor(props) {
+    super(props);
+    // state의 초깃값 설정하기
+    this.state = {
+      number: 0,
+      fixedNumber: 0,
+    };
+  }
   render() {
-    const { number, fixedNumber } = this.state; // state를 조회할 때는 this. state로 조회합니다.
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
     return (
       <div>
         <h1>{number}</h1>
-        <h2>{fixedNumber}</h2>
+        <h1>바뀌지 않는 값: {fixedNumber}</h1>
         <button
+          // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
-            this.setState({ number: number + 1 }, () => {
-              console.log('setState 호출');
-              console.log(this.state);
-            });
+            //this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
+            this.setState({ number: number + 1 });
           }}
         >
           +1
@@ -26,5 +36,4 @@ class Counter extends Component {
     );
   }
 }
-
 export default Counter;
